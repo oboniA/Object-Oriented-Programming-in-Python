83141e0f196392efda7c0d9f183adb282c6673a7
Oboni ANower <oboni_anower@yahoo.com>
2022-10-11 16:15:09 +0100

    more efficient

--- methods.py
+++ methods.py
@@ -4,17 +4,19 @@
     eg. want the full name of the student
 """
 
-class Student:  # Class created
+class Student:                                              # Class created
     def __init__(self, firstname, lastname, salary):        # created arguments
         # self.argument = instance_variable                 # argument & instance do not have to be the same
         self.firstname = firstname                          # setting the instance variables to the arguments
         self.lastname = lastname
         self.salary = salary
-        self.email = firstname + '.' + lastname + '@py.com' # notice: newly set here, wasn't an arguement
+        self.email = firstname + '.' + lastname + '@py.com' # notice: newly set here, wasn't an argument
 
+    def fullname(self):                                     # creating a method
+        return '{} {}'.format(self.firstname, self.lastname)
 
 std_1 = Student('Jamey', 'Kim', 2000 )
 std_2 = Student('Mimi', 'Stace', 2400)
 
-print('{} {}'.format(std_1.firstname, std_1.lastname))
-print('{} {}'.format(std_2.firstname, std_2.lastname))
+print(std_1.fullname())
+print(std_2.fullname())
